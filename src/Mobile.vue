04ddddd0ea5edbacc6e8abdb8b1b84ee113830df
âyejianghuai <921870425@qq.com>
2020-08-01 14:25:36 +0800

    resume

---
+++ src/Mobile.vue
@@ -0,0 +1,266 @@
+<template>
+  <div id="app">
+    <StyleEditor ref="styleEditor" :code="currentStyle"></StyleEditor>
+    <ResumeEditor ref="resumeEditor" :markdown="currentMarkdown" :enableHtml="enableHtml"></ResumeEditor>
+  </div>
+</template>
+
+<script>
+import StyleEditor from "./components/StyleEditor";
+import ResumeEditor from "./components/ResumeEditor";
+import "./assets/reset.css";
+
+export default {
+  name: "app",
+  components: {
+    StyleEditor,
+    ResumeEditor,
+  },
+  data() {
+    return {
+      interval: 40,
+      currentStyle: "",
+      enableHtml: false,
+      fullStyle: [
+        `/*
+* 尊敬的来客，你好
+* 这个是我的简历，请您收好
+*/
+/* 首先我准备给全局加点样式 */
+/* 先给所有元素加上过渡效果 */
+* {
+  transition: all .3s;
+}
+/* 白色背景太单调了，我们来点背景 */
+html {
+  color: rgb(222,222,222);
+  background: rgb(0,43,54);
+}
+/* 文字离边框太近了 */
+.styleEditor {
+  padding: .5em;
+  border: 1px solid;
+  overflow: auto;
+  width: 90vw;
+  margin: 2.5vh 5vw;
+  height: 90vh;
+}
+/* 太高了 */
+.styleEditor {
+  height: 45vh;
+}
+/* 代码高亮 */
+.token.selector{
+  color: rgb(133,153,0);
+}
+.token.property{
+  color: rgb(187,137,0);
+}
+.token.punctuation{
+  color: yellow;
+}
+.token.function{
+  color: rgb(42,161,152);
+}
+
+/* 加点 3D 效果呗 */
+html{
+  perspective: 1000px;
+}
+.styleEditor {
+  position: fixed; left: 0; top: 0;
+  transform: rotateX(-10deg) translateZ(-50px) ;
+}
+
+/* 接下来我给自己准备一个编辑器 */
+.resumeEditor{
+  position: fixed;
+  top: 50%; left: 0;
+  padding: .5em;  margin: 2.5vh;
+  width: 95vw; height: 45vh;
+  border: 1px solid;
+  background: white; color: #222;
+  overflow: auto;
+}
+/* 好了，我开始写简历了 */
+
+
+`,
+        `
+/* 简历的大致内容完成啦
+ * 对了，这是 Markdown 格式的，我需要变成对 HR 更友好的格式
+ */
+`,
+        `
+/* 再对 HTML 加点样式 */
+.resumeEditor{
+  padding: 2em;
+}
+.resumeEditor h2{
+  display: inline-block;
+  border-bottom: 1px solid;
+  margin: 1em 0 .5em;
+}
+.resumeEditor ul,.resumeEditor ol{
+  list-style: none;
+}
+.resumeEditor ul> li::before{
+  content: '•';
+  margin-right: .5em;
+}
+.resumeEditor ol {
+  counter-reset: section;
+}
+.resumeEditor ol li::before {
+  counter-increment: section;
+  content: counters(section, ".") " ";
+  margin-right: .5em;
+}
+.resumeEditor blockquote {
+  margin: 1em;
+  padding: .5em;
+  background: #ddd;
+}
+`,
+      ],
+      currentMarkdown: "",
+      fullMarkdown: `叶江怀
+----
+
+现在**好邦妮家政服务公司**就职**前端开发**岗位。
+
+个人资料
+----
+* 姓  名：叶江怀　　　　学  历：本科　　　　　学  校：福建农林大学 
+* 学  院：东方学院　　　专  业：软件工程               
+* 出生年月：1995.11　　户  口：厦门同安　　　手  机：18106081023
+* 微  信：18106081023　邮  箱：921870425@qq.com
+
+技能
+----
+
+* 1、熟练使用JavaScript、Vue框架的使用
+* 2、熟练使用elementUI 、Antd 、Vant 等UI框架的使用
+* 3、熟练使用Photoshop、pxcook、svn 、git、vscode、npm、yarn、XMind等相关工具
+* 4、了解并手写过vue底层指令的简单实现原理，会sass其他使用方法
+* 5、具有团队精神，懂得跟着团队走
+
+工作经历
+----
+
+2019.11 - 至今　　　　好邦妮家政服务公司　　　　前端开发
+- 好邦妮保姆大本营家政**CRM系统**，本人负责前端，后端为C#。
+该系统提高了公司经纪人分配保姆、月嫂等工人的效率，为财务，人事提供了更便捷的操作。
+- 我们的技术栈为：**vue、typescript、vuex、echarts、scoket、axios、sass、eslint**等。
+- 在产品确定之后，以elementUI为UI基准进行开发，其中对该UI框架的表单组件进行了二次封装，成为符合本公司风格的一套form表单组件。
+做好eslint标准，严格遵守约束好的各项标准，如命名规则，代码风格。做好系统性能上的优化，如将部分枚举接口存放到**vuex**，将图标换成svg等操作。
+- 目前该系统已基本成熟。
+
+我的项目
+----
+
+* [个人博客](https://yejianghuai.github.io/docs/)
+* [github](https://github.com/Yejianghuai)
+
+`,
+    };
+  },
+  created() {
+    this.makeResume();
+  },
+
+  methods: {
+    makeResume: async function () {
+      await this.progressivelyShowStyle(0);
+      await this.progressivelyShowResume();
+      await this.progressivelyShowStyle(1);
+      await this.showHtml();
+      await this.progressivelyShowStyle(2);
+    },
+    showHtml: function () {
+      return new Promise((resolve, reject) => {
+        this.enableHtml = true;
+        this.$nextTick(() => {
+          this.$refs.resumeEditor.goTop();
+        });
+        resolve();
+      });
+    },
+    progressivelyShowStyle(n) {
+      return new Promise((resolve, reject) => {
+        let interval = this.interval;
+        let showStyle = async function () {
+          let style = this.fullStyle[n];
+          if (!style) {
+            return;
+          }
+          // 计算前 n 个 style 的字符总数
+          let length = this.fullStyle
+            .filter((_, index) => index <= n)
+            .map((item) => item.length)
+            .reduce((p, c) => p + c, 0);
+          let prefixLength = length - style.length;
+          if (this.currentStyle.length < length) {
+            let l = this.currentStyle.length - prefixLength;
+            let char = style.substring(l, l + 1) || " ";
+            this.currentStyle += char;
+            if (style.substring(l - 1, l) === "\n" && this.$refs.styleEditor) {
+              this.$nextTick(() => {
+                this.$refs.styleEditor.goBottom();
+              });
+            }
+            setTimeout(showStyle, interval);
+          } else {
+            resolve();
+          }
+        }.bind(this);
+        showStyle();
+      });
+    },
+    progressivelyShowResume() {
+      return new Promise((resolve, reject) => {
+        let length = this.fullMarkdown.length;
+        let interval = this.interval;
+        let showResume = () => {
+          if (this.currentMarkdown.length < length) {
+            this.currentMarkdown = this.fullMarkdown.substring(
+              0,
+              this.currentMarkdown.length + 1
+            );
+            let lastChar = this.currentMarkdown[
+              this.currentMarkdown.length - 1
+            ];
+            let prevChar = this.currentMarkdown[
+              this.currentMarkdown.length - 2
+            ];
+            if (prevChar === "\n" && this.$refs.resumeEditor) {
+              this.$nextTick(() => this.$refs.resumeEditor.goBottom());
+            }
+            setTimeout(showResume, interval);
+          } else {
+            resolve();
+          }
+        };
+        showResume();
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  min-height: 100vh;
+  position: relative;
+}
+
+html {
+  min-height: 100vh;
+}
+* {
+  box-sizing: border-box;
+}
+</style>
